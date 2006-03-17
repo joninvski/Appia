aeb59a473e67c2c2355be4e444dfce0fbd888490
nunomrc <nunomrc@fd6ec0a9-dc39-0410-a308-e6a400858ce9>
2006-03-17 20:10:03 +0000

    *** empty log message ***
    git-svn-id: https://appia.svn.sourceforge.net/svnroot/appia@3 fd6ec0a9-dc39-0410-a308-e6a400858ce9

--- trunk/appia/bin/gossipServer.sh
+++ trunk/appia/bin/gossipServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# java -classpath $CLASSPATH org.continuent.appia.demo.ExecuteXML ./config/gossipServer.xml
+#java -classpath $CLASSPATH org.continuent.appia.demo.ExecuteXML ./config/gossipServer.xml
 
 java -classpath $CLASSPATH org.continuent.appia.gossip.GossipServer "$@"
