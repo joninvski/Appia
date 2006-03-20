0be5fd85424910a7f1b4390f14d5a5a5d8ba4bd9
nunomrc <nunomrc@fd6ec0a9-dc39-0410-a308-e6a400858ce9>
2006-03-20 17:43:42 +0000

    fixed configuration files path.
    git-svn-id: https://appia.svn.sourceforge.net/svnroot/appia@6 fd6ec0a9-dc39-0410-a308-e6a400858ce9

--- trunk/appia/bin/validate.sh
+++ trunk/appia/bin/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting group member 1"
-java -classpath $CLASSPATH org.continuent.appia.demo.ExecuteXML ./config/perf.xml &
+java -classpath $CLASSPATH org.continuent.appia.demo.ExecuteXML ./config/demo/perf.xml &
 echo "Starting group member 2"
-java -classpath $CLASSPATH org.continuent.appia.demo.ExecuteXML ./config/perf.xml
+java -classpath $CLASSPATH org.continuent.appia.demo.ExecuteXML ./config/demo/perf.xml
 echo "Test finished"
