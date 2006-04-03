c7d61f4381df60417c50ff1e9af83eec7f6f5159
nunomrc <nunomrc@fd6ec0a9-dc39-0410-a308-e6a400858ce9>
2006-04-03 17:28:18 +0000

    Added events to handle copy-free optimistic deliveries of messages.
    git-svn-id: https://appia.svn.sourceforge.net/svnroot/appia@12 fd6ec0a9-dc39-0410-a308-e6a400858ce9

---
+++ trunk/appia/src/org/continuent/appia/protocols/common/ServiceEvent.java
@@ -0,0 +1,51 @@
+/**
+ * Appia: Group communication and protocol composition framework library
+ * Copyright 2006 University of Lisbon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ *
+ * Initial developer(s): Alexandre Pinto and Hugo Miranda.
+ * Contributor(s): See Appia web page for a list of contributors.
+ */
+
+package org.continuent.appia.protocols.common;
+
+import org.continuent.appia.core.AppiaEventException;
+import org.continuent.appia.core.Channel;
+import org.continuent.appia.core.Event;
+import org.continuent.appia.core.Session;
+
+public class ServiceEvent extends Event {
+	
+	private Object messageID;
+
+	public ServiceEvent(Object mid) {
+		super();
+		messageID = mid;
+	}
+
+	public ServiceEvent(Channel channel, int dir, Session src, Object mid)
+			throws AppiaEventException {
+		super(channel, dir, src);
+		messageID = mid;
+	}
+
+	public Object getMessageID() {
+		return messageID;
+	}
+
+	public void setMessageID(Object messageID) {
+		this.messageID = messageID;
+	}
+
+}
