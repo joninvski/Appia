763a1076a016019b2564d6d556178df747a468d9
nunomrc <nunomrc@fd6ec0a9-dc39-0410-a308-e6a400858ce9>
2006-07-18 15:49:47 +0000

    Fixes APPIA-3 Added these two methods again to maintain some compatibility with previous releases. The methods are also deprecated and will be out when InetWithPort is also out.
    git-svn-id: https://appia.svn.sourceforge.net/svnroot/appia@59 fd6ec0a9-dc39-0410-a308-e6a400858ce9

--- trunk/appia/src/org/continuent/appia/protocols/utils/ParseUtils.java
+++ trunk/appia/src/org/continuent/appia/protocols/utils/ParseUtils.java
@@ -27,6 +27,8 @@ import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.text.ParseException;
 
+import org.continuent.appia.protocols.common.InetWithPort;
+
 /**
  * Class containing some methods usefull for parsing strings.
  * 
@@ -48,7 +50,7 @@ public final class ParseUtils {
    * @throws ParseException
    * @throws UnknownHostException
    */
-  public static InetSocketAddress[] parseInetWithPortArray(String s, InetAddress default_host, int default_port) 
+  public static InetSocketAddress[] parseSocketAddressArray(String s, InetAddress default_host, int default_port) 
   throws ParseException, UnknownHostException {
     //System.err.println("##### Parse string: "+s);
     
@@ -79,7 +81,7 @@ public final class ParseUtils {
       if (isep > previsep+1) {
         try {
           //System.err.print("##### \""+s.substring(previsep+1,isep)+"\"");
-          result[j++]=parseInetWithPort(s.substring(previsep+1,isep),default_host,default_port);
+          result[j++]=parseSocketAddress(s.substring(previsep+1,isep),default_host,default_port);
           //System.err.println(" -> "+result[j-1]);
         } catch (ParseException ex) {
           throw new ParseException(ex.getMessage(),previsep+1+ex.getErrorOffset());
@@ -102,7 +104,7 @@ public final class ParseUtils {
    * @throws ParseException
    * @throws UnknownHostException
    */
-  public static InetSocketAddress parseInetWithPort(String s, InetAddress default_host, int default_port) 
+  public static InetSocketAddress parseSocketAddress(String s, InetAddress default_host, int default_port) 
   throws ParseException, UnknownHostException {
       InetSocketAddress addr = null;
     int iport=s.indexOf(':');
@@ -122,5 +124,93 @@ public final class ParseUtils {
     
     return addr;
   }
+  
+  /** 
+   * Generate a InetWithPort[] from a string of the form "[host][:port][,[host][:port] ...]".
+   * 
+   * @param s
+   * @param default_host
+   * @param default_port
+   * @return an array with addresses
+   * @see InetWithPort
+   * @throws ParseException
+   * @throws UnknownHostException
+   * @deprecated
+   */
+  public static InetWithPort[] parseInetWithPortArray(String s, InetAddress default_host, int default_port) throws ParseException, UnknownHostException {
+      //System.err.println("##### Parse string: "+s);
+      
+      char SEP=',';
+      int isep=-1;
+      int previsep;
+      int j;
+
+      int count=1;
+      while (isep < s.length()) {
+        isep=s.indexOf(SEP,isep+1);
+        if (isep < 0)
+          break;
+        count++;
+      }
+      InetWithPort[] result=new InetWithPort[count];
+   
+      //System.err.println("##### Parsed array("+result.length+"):{");
+
+      j=0;
+      isep=-1;
+      while (isep < s.length()) {
+        previsep=isep;
+        isep=s.indexOf(SEP,previsep+1);
+        if (isep < 0)
+          isep=s.length();
+        
+        if (isep > previsep+1) {
+          try {
+            //System.err.print("##### \""+s.substring(previsep+1,isep)+"\"");
+            result[j++]=parseInetWithPort(s.substring(previsep+1,isep),default_host,default_port);
+            //System.err.println(" -> "+result[j-1]);
+          } catch (ParseException ex) {
+            throw new ParseException(ex.getMessage(),previsep+1+ex.getErrorOffset());
+          }
+        } else {
+          throw new ParseException("Missing element in array.",previsep+1);
+        }
+      }
+      
+      return result;
+    }
+
+    /**
+     * Generate a InetWithPort from a string of the form "[host][:port]".
+     *  
+     * @param s
+     * @param default_host Host to use if the string doesn't contain a host part. If <b>null</b> string must contain a host part.
+     * @param default_port Port to use if the string doesn't contain a port part. If <b>-1</b> string must contain a host part.
+     * @return a parsed address
+     * @see InetWithPort
+     * @throws ParseException
+     * @throws UnknownHostException
+     * @deprecated
+     */
+    public static InetWithPort parseInetWithPort(String s, InetAddress default_host, int default_port) throws ParseException, UnknownHostException {
+      InetWithPort addr=null;
+      
+      int iport=s.indexOf(':');
+      if (iport < 0) {
+        if (default_port < 0)
+          throw new ParseException("Missing port in \""+s+"\"",0);
+        addr = new InetWithPort(InetAddress.getByName(s),default_port);
+      } else if (iport == 0) {
+        if (default_host == null)
+          throw new ParseException("Missing host in \""+s+"\"",iport);
+        addr = new InetWithPort(default_host,Integer.parseInt(s.substring(1)));
+      } else if (iport < s.length()-1) {
+          addr = new InetWithPort(InetAddress.getByName(s.substring(0,iport)),Integer.parseInt(s.substring(iport+1)));
+      } else {
+        throw new ParseException("Missing port in \""+s+"\"",iport);
+      }
+      
+      return addr;
+    }
 }
 
