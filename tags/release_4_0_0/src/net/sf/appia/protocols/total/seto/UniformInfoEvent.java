aadbaf0487e48f67bd210ba94f11f8d22aae1724
nunomrc <nunomrc@fd6ec0a9-dc39-0410-a308-e6a400858ce9>
2007-10-29 17:56:20 +0000

    first release in the source forge.
    git-svn-id: https://appia.svn.sourceforge.net/svnroot/appia@311 fd6ec0a9-dc39-0410-a308-e6a400858ce9

---
+++ tags/release_4_0_0/src/net/sf/appia/protocols/total/seto/UniformInfoEvent.java
@@ -0,0 +1,30 @@
+
+/**
+ * Appia: Group communication and protocol composition framework library
+ * Copyright 2006 University of Lisbon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ *
+ * Initial developer(s):Jose Mocito.
+ * Contributor(s): See Appia web page for a list of contributors.
+ */
+ package net.sf.appia.protocols.total.seto;
+
+import net.sf.appia.protocols.group.events.GroupSendableEvent;
+
+/**
+ * Event used to exchange the uniformity information of messages flowing in the group.
+ * 
+ */
+public class UniformInfoEvent extends GroupSendableEvent {
+}
