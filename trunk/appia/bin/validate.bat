05ab33e6b21b91927a6b1001fbc9a4cfa79871fd
nunomrc <nunomrc@fd6ec0a9-dc39-0410-a308-e6a400858ce9>
2007-11-12 10:28:22 +0000

    Fixed an issue in the validate scripts.
    git-svn-id: https://appia.svn.sourceforge.net/svnroot/appia@317 fd6ec0a9-dc39-0410-a308-e6a400858ce9

--- trunk/appia/bin/validate.bat
+++ trunk/appia/bin/validate.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Starting group member 1
-start java org.continuent.appia.demo.ExecuteXML config\demo\perf.xml
+start java net.sf.appia.demo.ExecuteXML config\demo\perf.xml
 echo Starting group member 2
-start java org.continuent.appia.demo.ExecuteXML config\demo\perf.xml
+start java net.sf.appia.demo.ExecuteXML config\demo\perf.xml
 echo Test finished
--- trunk/appia/bin/validate.sh
+++ trunk/appia/bin/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting group member 1"
-java org.continuent.appia.demo.ExecuteXML ./config/demo/perf.xml &
+java net.sf.appia.demo.ExecuteXML ./config/demo/perf.xml &
 echo "Starting group member 2"
-java org.continuent.appia.demo.ExecuteXML ./config/demo/perf.xml
+java net.sf.appia.demo.ExecuteXML ./config/demo/perf.xml
 echo "Test finished"
